02edfb7bdcf4d11d631b82d7192ee1e558307b7e
yannevic <nanny.win15@gmail.com>
2026-08-12 17:17:26 -0300

    feat: novos pacotes de cartas adicionados: lol e lucifer

--- src/assets/cards/packs/index.ts
+++ src/assets/cards/packs/index.ts
@@ -1,11 +1,15 @@
 import pacoteComum from './pacote-comum.png'
 import pacoteJardimSecreto from './pacote-jardimsecreto.png'
 import pacoteDexter from './pacote-dexter.png'
+import pacoteLucifer from './pacote-lucifer.png'
+import pacoteLol from './pacote-lol.png'
 // arte do pacote promocional, uma por coleção. Sempre que uma coleção nova
 // for cadastrada em lib/cards.ts, adicionar a arte dela aqui também.
 export const PROMO_PACK_ART: Record<string, string> = {
   'jardim-secreto': pacoteJardimSecreto,
   'dexter-serie': pacoteDexter,
+  'lucifer-serie': pacoteLucifer,
+  'lol-jogo': pacoteLol,
 }
 
 export const PACK_ART = {
